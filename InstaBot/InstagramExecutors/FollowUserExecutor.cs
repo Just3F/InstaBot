15efb9d9b39d293afd520c82d8a208b59999897d
Dmitry Vasilyuk <dmitriy.vasilyuk@fortegrp.net>
2018-09-25 20:17:24 +0300

    Added web version

--- InstaBot/InstagramExecutors/FollowUserExecutor.cs
+++ InstaBot/InstagramExecutors/FollowUserExecutor.cs
@@ -15,15 +15,40 @@ namespace InstaBot.Service.InstagramExecutors
 
         public async Task Execute(Queue queue, InstaBotContext db)
         {
-            var instaExploreFeed = await _instaApi.GetExploreFeedAsync(PaginationParameters.MaxPagesToLoad(0));
-            var firstInstaMedia = instaExploreFeed?.Value?.Medias?.FirstOrDefault();
-
-            if (firstInstaMedia != null)
+            long userId = 0;
+            if (queue.LoadId != null)
             {
-                await _instaApi.FollowUserAsync(firstInstaMedia.User.Pk);
+                userId = await GetUserIdByTag(queue, userId);
+            }
+            else
+            {
+                userId = await GetUserIdFromFeed(userId);
+            }
+
+            if (userId != 0)
+            {
+                await _instaApi.FollowUserAsync(userId);
                 await UpdateQueueLastActivityAsync(queue, db);
                 Console.WriteLine("FollowUserExecutor for " + queue.User.Name);
             }
+        }
+
+        private async Task<long> GetUserIdFromFeed(long userId)
+        {
+            var instaExploreFeed = await _instaApi.GetExploreFeedAsync(PaginationParameters.MaxPagesToLoad(0));
+            var firstInstaMedia = instaExploreFeed?.Value?.Medias?.FirstOrDefault();
+            if (firstInstaMedia != null)
+                userId = firstInstaMedia.User.Pk;
+            return userId;
+        }
+
+        private async Task<long> GetUserIdByTag(Queue queue, long userId)
+        {
+            var mediasByTag = await _instaApi.GetTagFeedAsync(queue.LoadId, PaginationParameters.MaxPagesToLoad(0));
+            var user = mediasByTag.Value.Medias.Where(x => !x.User.FriendshipStatus.Following && !x.User.IsPrivate).Select(x => x.User).FirstOrDefault();
+            if (user != null)
+                userId = user.Pk;
+            return userId;
         }
     }
 }
